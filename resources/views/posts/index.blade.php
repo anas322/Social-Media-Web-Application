f1bd372cd02ec66461824526827caf6989b95be6
Anas <anaselnahef12@gmail.com>
2022-11-02 01:34:05 +0200

    add the left side bar ui and add the post likes, comment and share section

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -34,12 +34,82 @@
         </div>
         </header>
         
-     <section class="grid grid-cols-12 gap-x-6 my-2">
+     <section class="grid grid-cols-12 gap-x-6 my-2 auto-rows-min">
             <!-- side left -->
-            <aside class="bg-slate-300 col-span-3 rounded-2xl"></aside>
+            <aside class=" bg-white col-span-3 row-span-2  rounded-2xl">
+                <div class='flex flex-col gap-x-4'>
+
+            
+                        <div class='flex items-center space-x-10 py-6 pl-4 '> 
+                            <div><img src="{{$userProfilePic}}" style='clip-path : circle()' class='block h-12 w-10'></div>
+                            <div>
+                                <p><strong>{{auth()->user()->name}}</strong></p>
+                                <p class='text-gray-400'>{{auth()->user()->email}}</p>
+                            </div>
+                        </div>
+             
+
+                        <div class='flex items-center space-x-10 py-6 pl-4  bg-gray-100 '>
+                            <div>
+                                <object data="{{asset('images/home-icon.svg')}}" class="block h-8 w-auto" ></object>
+                            </div>
+                            <strong class='capitalize'>home</strong>
+                        </div>
+
+                        <div class='flex items-center space-x-10 py-6 pl-4 '>
+                            <div>
+                                <object data="{{asset('images/explore.svg')}}" class="block h-8 w-auto" ></object>
+                            </div>
+                            <strong class='capitalize'>explore</strong>
+                        </div>
+
+                        <div class='flex items-center space-x-10 py-6 pl-4 '>
+                            <div class='relative'>
+                                <object data="{{asset('images/notify.svg')}}" class="block h-8 w-auto" ></object>
+                                  <span class="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+                                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                                        <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                                    </span>
+                            </div>
+                                <strong class='capitalize'>notifications </strong>
+                        </div>
+
+                        <div class='flex items-center space-x-10 py-6 pl-4 '>
+                            <div class='relative'>
+                                <object data="{{asset('images/messages.svg')}}" class="block h-8 w-auto" ></object>
+                                <span class="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+                                        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                                        <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                                    </span>
+                            </div>
+                            <strong class='capitalize'>messages</strong>
+                        </div>
+
+                        <div class='flex items-center space-x-10 py-6 pl-4 '>
+                            <div>
+                                <object data="{{asset('images/bookmarks.svg')}}" class="block h-8 w-auto" ></object>
+                            </div>
+                            <strong class='capitalize'>bookmarks</strong>
+                        </div>
+
+                        <div class='flex items-center space-x-10 py-6 pl-4 '>
+                            <div>
+                                <object data="{{asset('images/theme.svg')}}" class="block h-11 w-auto" ></object>
+                            </div>
+                            <strong class='capitalize'>theme</strong>
+                        </div>
+
+                        <div class='flex items-center space-x-10 py-6 pl-4 '>
+                            <div>
+                                <object data="{{asset('images/settings.svg')}}" class="block h-8 w-auto" ></object>
+                            </div>
+                            <strong class='capitalize'>settings</strong>
+                        </div>
+                </div>
+            </aside>
 
             <!-- main  -->
-            <main class="col-span-6">
+            <main class="col-span-6 row-span-6">
                 <!-- create post -->
                 <div class="mb-4 px-4 py-3 bg-white rounded-full box-border">
                      <form>
@@ -73,10 +143,50 @@
                                 <div>
                                     <img src="{{ asset('storage') . '/' . $post->image  }}" class="rounded-xl w-full">
                                 </div>
+
+                                <!-- love, comment, share, bookmark, section -->
+                                <div class='flex justify-between'>
+                                   <div class='flex space-x-4 pl-4'>
+                                        <div>
+                                            <object data="{{asset('images/heart.svg')}}" class="block h-8 w-auto" ></object>
+                                        </div>
+
+                                        <div>
+                                            <object data="{{asset('images/comments.svg')}}" class="block h-8 w-auto" ></object>
+                                        </div>
+
+                                        <div>
+                                            <object data="{{asset('images/share.svg')}}" class="block h-8 w-auto" ></object>
+                                        </div>
+                                    </div>
+
+                                    <div class='pr-2'>
+                                        <object data="{{asset('images/bookmarks.svg')}}" class="block h-8 w-auto" ></object>
+                                    </div>
+                                </div>
+
+                                <!-- love list section  -->
+                                <div class='flex items-center space-x-2 '>
+                                    <div class='flex pl-5'>
+                                        <div class = ' -ml-4 p-1' >
+                                            <div  > <img src="{{ $post->user->profile_photo_path ? asset('storage/' . $post->user->profile_photo_path) : asset('storage') . '/default/default.png' }}"  style="clip-path:circle()"  class="w-7"></div>
+                                        </div>
+    
+                                         <div class = ' -ml-4  p-1'>
+                                            <div style="clip-path:circle()"> <img src="{{ $post->user->profile_photo_path ? asset('storage/' . $post->user->profile_photo_path) : asset('storage') . '/default/default.png' }}"  class="w-7"></div>
+                                        </div>
+    
+                                         <div class = ' -ml-4  p-1'>
+                                            <div style="clip-path:circle()"> <img src="{{ $post->user->profile_photo_path ? asset('storage/' . $post->user->profile_photo_path) : asset('storage') . '/default/default.png' }}"  class="w-7"></div>
+                                        </div>
+                                    </div>
+                                    
+                                    <p class='text-lg'>liked by <strong>emlia achiever</strong> and <strong>2,357 others</strong> </p>
+                                </div>
                                 
                                 <!-- caption  -->
                                 <div>
-                                    <p class="text-sm text-gray-600 font-semibold"> <strong class="text-base text-black pr-2">{{$post->user->name}}</strong>{{$post->caption}}</p>
+                                    <p class="text-gray-600"> <strong class="text-black pr-2">{{$post->user->name}}</strong>{{$post->caption}}</p>
                                 </div>
                             </article>
                         @endforeach
@@ -86,7 +196,8 @@
             </main>
 
             <!-- side right  -->
-            <aside class="bg-slate-300 col-span-3 rounded-2xl"></aside>
+            <aside class="bg-slate-300 col-span-3 row-span-2 rounded-2xl">
+            </aside>
         </section>
 </body>
 </html>
