2f5834012e0e050d03967c3ef93377f27b616e29
Anas <anaselnahef12@gmail.com>
2022-11-09 14:51:02 +0200

    add spinging loading in search users

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -10,7 +10,6 @@
         @vite(['resources/css/app.css'])
 
         <script>
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark');
     } else {
@@ -45,6 +44,17 @@
 
                     <!-- searched users -->
                     <div id="searched-users" class="absolute top-10 left-0 right-0 bg-gray-100 dark:bg-gray-500 z-50 rounded-b-lg max-h-60 overflow-y-auto transition duration-500">
+                        <div class="text-center py-4 hidden " id="loading-spining">
+                            <div role="status">
+                                <svg class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                </svg>
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                        <div id="searched-users-data">
+                        </div>
                     </div>
                  
                 </div>
@@ -233,9 +243,17 @@
 
                 $('#search-creators').keyup( function (e) {
                     //reset users
-                    $('#searched-users').html(null)
+                    $('#searched-users-data').html(null)
+
+                    //show spining 
+                    $("#loading-spining").removeClass('hidden');
 
                     let name = $(this).val();
+                    
+                    //remove spining if the field is empty
+                    if (!name) {
+                         $("#loading-spining").addClass('hidden');
+                    }
 
                     if(name.trim()){
                         
@@ -244,9 +262,12 @@
                             url: "{{route('users.search')}}",
                             data: {name},
                             success:(result)=>{
+                                //remove loading spining
+                                 $("#loading-spining").addClass('hidden');
+
                                 // get the user and the insert them as html 
                                 result.users.forEach(user => {
-                                        $('#searched-users').append(`
+                                        $('#searched-users-data').append(`
                                             <a href="{{ url('/profile') }}/${user.id}">
                                             <div class="flex items-center space-x-4 p-3 hover:bg-gray-200 dark:hover:bg-slate-600 rounded-lg">
                                                 <div>
@@ -258,6 +279,10 @@
                                     `)
                                 });
                              
+                            },
+                            error:()=>{
+                                 $("#loading-spining").addClass('hidden');
+                                  $('#searched-users').append(`<p class="text-center text-red-600 p-2 font-medium"> ERROR: something went wrong! :(`)
                             }
                         });
                     }
