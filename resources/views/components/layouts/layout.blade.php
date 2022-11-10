46eb17b0112fbc3d5c0302c6f81b2a57652675b1
Anas <anaselnahef12@gmail.com>
2022-11-10 16:20:04 +0200

    some extra style

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -117,7 +117,7 @@
 
                         </div>
 
-                        <div  class="flex items-center justify-around space-x-2">
+                        <div  class="flex items-center justify-around space-x-2 py-4 mx-4 bg-gray-100 dark:bg-gray-600 rounded-md ">
                         
                             <button id="theme-toggle-1" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 ">
                                 <svg id="theme-toggle-dark-icon-1" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
